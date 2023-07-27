6a6ab52e446c9419856b7ecadf442d4b55896760
Dan <danielross1995@gmail.com>
2023-07-27 21:52:43 +0200

    fixes

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from PIL import Image
+import os
 
 def remove_green_screen(input_path, output_path):
     # Load the image
@@ -10,23 +11,21 @@ def remove_green_screen(input_path, output_path):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # Define color of green screen in BGR
-    green_screen_color_bgr = np.uint8([[[0, 208, 28]]])  # BGR representation of your green screen color
+    green_screen_color_bgr = np.uint8([[[0, 220, 0]]]) 
 
     # Convert the green screen color to HSV
     green_screen_hsv = cv2.cvtColor(green_screen_color_bgr, cv2.COLOR_BGR2HSV)
 
     # Define range of green color in HSV
-    lower_green_hsv = np.array([green_screen_hsv[0][0][0] - 20, 50, 50])
-    upper_green_hsv = np.array([green_screen_hsv[0][0][0] + 20, 255, 255])
+    lower_green_hsv = np.array([green_screen_hsv[0][0][0] - 35, 100, 100])
+    upper_green_hsv = np.array([green_screen_hsv[0][0][0] + 35, 255, 255])
 
     # Threshold the HSV image to get only green colors
     mask = cv2.inRange(hsv, lower_green_hsv, upper_green_hsv)
 
-    # Desaturate green pixels
-    hsv[mask > 0] = [0, 0, 255]
-
-    # Convert back to BGR for saving
-    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    # Morphological operations
+    mask = cv2.dilate(mask, None, iterations=1)
+    mask = cv2.erode(mask, None, iterations=1)
 
     # Create an alpha channel with the same shape as the green screen mask
     alpha_channel = np.ones(mask.shape, dtype=np.uint8) * 255
@@ -35,11 +34,30 @@ def remove_green_screen(input_path, output_path):
     alpha_channel[mask > 0] = 0
 
     # Add the alpha channel to the image
-    img = cv2.merge((bgr, alpha_channel))
+    img = cv2.merge((img, alpha_channel))
 
-    # Save it with transparency
+    # Convert the image from OpenCV's default BGR format to RGB, and save it with transparency
     Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)).save(output_path)
 
+# Directory containing the input images
+input_dir = 'input/'
 
-# Testing the function
-remove_green_screen('test.png', 'output1.png')
+# Directory to save the output images
+output_dir = 'output/'
+
+# List of all image files in the input directory
+image_files = [f for f in os.listdir(input_dir) if f.endswith('.png')]
+
+# Total number of images
+total_images = len(image_files)
+
+# Iterate over all the images in the input directory
+for i, filename in enumerate(image_files):
+    # Full path to the input image
+    input_path = os.path.join(input_dir, filename)
+    # Full path to save the output image
+    output_path = os.path.join(output_dir, filename)
+    # Remove the green screen
+    remove_green_screen(input_path, output_path)
+    # Print progress
+    print(f'Processing file {i+1} of {total_images}: {filename}')
